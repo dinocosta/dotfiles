14c5a72f887e601eab5973253802f5123ab35396
dino <dinojoaocosta@gmail.com>
2026-05-06 22:41:24 +0100

    chore: add zed fzf themes

---
+++ fzf/zed_dark.sh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="
+	--color=fg:#6A7587,bg:#101419,hl:#e06c75
+	--color=fg+:#C6CAD0,bg+:#1a1d23,hl+:#e06c75
+	--color=border:#262C36,header:#61afef,gutter:#101419
+	--color=spinner:#d19a66,info:#739EE8
+	--color=pointer:#c678dd,marker:#fc6a67,prompt:#6A7587"
---
+++ fzf/zed_light.sh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="
+	--color=fg:#6A7587,bg:#DFE3EC,hl:#E4516A
+	--color=fg+:#0C0F13,bg+:#FFFFFF,hl+:#E4516A
+	--color=border:#CED3DF,header:#3480DF,gutter:#DFE3EC
+	--color=spinner:#DF8251,info:#0599BD
+	--color=pointer:#AB62E8,marker:#d36151,prompt:#6A7587"
